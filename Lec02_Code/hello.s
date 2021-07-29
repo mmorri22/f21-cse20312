8a0b0f12a9ddb9ecb8489f6bf0ef25eb98a6a40a
Matthew Morrison <mmorri22@nd.edu>
2021-07-29 10:00:28 -0400

    Update Lec 02

---
+++ Lec02_Code/hello.s
@@ -0,0 +1,30 @@
+	.file	"hello.c"
+	.section	.rodata
+.LC0:
+	.string	"Hello, World\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	stdout(%rip), %rax
+	movq	%rax, %rcx
+	movl	$13, %edx
+	movl	$1, %esi
+	movl	$.LC0, %edi
+	call	fwrite
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-39)"
+	.section	.note.GNU-stack,"",@progbits
