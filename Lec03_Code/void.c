052a24bb4ce01508815e68472a225e87f7a1475b
Matthew Morrison <mmorri22@nd.edu>
2021-07-29 16:16:55 -0400

    Lec03 initial

---
+++ Lec03_Code/void.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	
+	/* Deliberate logic error to show students importance of data types */
+	int size = -1 * 13 * sizeof(char);
+
+	void* hello = malloc( size );
+
+	fprintf( stdout, "Working Memory: %p\n", &hello );
+	fprintf( stdout, "Long-term Memory: %p\n", hello );
+
+	// fprintf(stdout, "Hello, World\n");
+	
+	free( hello );
+	
+	return 0;
+}
