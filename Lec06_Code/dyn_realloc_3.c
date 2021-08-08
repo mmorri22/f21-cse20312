965a6cde1a224a7b69142d71e85e4bf72e3c8f74
Matthew Morrison <mmorri22@nd.edu>
2021-08-08 17:56:02 -0400

    Lecture 06 Setup

---
+++ Lec06_Code/dyn_realloc_3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( void ){
+
+	long unsigned int init_len = 5;
+	int* dyn_int_array = (int *)malloc( init_len * sizeof(int) );
+	int* dyn_int_array_2 = (int *)malloc( init_len * sizeof(int) );
+	
+	fprintf( stdout, "array_1 at %p, array_2 at %p\n", dyn_int_array, dyn_int_array_2 );
+
+	char read_char = 'y';
+	long unsigned int num_read = 0;
+
+	while( read_char == 'y' ){
+
+		int read_int;
+		fprintf( stdout, "Enter an integer: ");
+		fscanf( stdin, "%d", &read_int );
+
+		dyn_int_array[ num_read ] = read_int;
+
+		++num_read;	
+
+		long unsigned int iter;
+		for( iter = 0; iter < num_read; ++iter ){
+		
+			fprintf( stdout, "Allocated length = %lu, dyn_int_array[ %lu ] = %d,\t&dyn_int_array[ %lu ] = %p \n", 
+					init_len, iter, dyn_int_array[iter], iter, &dyn_int_array[iter] );
+				
+		}
+		
+		// Initial attempt to re-allocate 
+		if( num_read == init_len ){
+			init_len *= 2;
+			dyn_int_array = (int *)realloc( dyn_int_array, init_len ); 
+		}
+
+		// Flush the input buffer
+		getchar();
+
+		fprintf( stdout, "Do you wish to continue? (y for yes): ");  
+		fscanf( stdin, "%c", &read_char ); 
+	}
+
+	// Free the Dynamically Allocated Arrays
+	free( dyn_int_array );
+	free( dyn_int_array_2 );
+
+	return 0;
+
+}
