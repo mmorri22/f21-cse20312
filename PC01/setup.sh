e938d2b32cfcf357611d5c971beece049cd861a7
Matthew Morrison <mmorri22@nd.edu>
2021-08-30 05:10:12 -0400

    PC01 setup.sh Fix

--- PC01/setup.sh
+++ PC01/setup.sh
@@ -4,3 +4,4 @@ wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/PC01/sudoku.h
 wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/PC01/puzzle1.txt
 wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/PC01/puzzle2.txt
 wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/PC01/puzzle3.txt
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/PC01/Makefile
