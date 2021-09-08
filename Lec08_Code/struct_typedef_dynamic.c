424e034ad4d271fd2362bd075a0a9c9475e1b5b0
Matthew Morrison <mmorri22@nd.edu>
2021-09-08 07:04:49 -0400

    Update Test Script

--- Lec08_Code/struct_typedef_dynamic.c
+++ Lec08_Code/struct_typedef_dynamic.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct simple{
+typedef struct{
 	
 	int int_data;
 	double double_data;
 	
-};
+} simple;
 
 int main(){
 	
 	/* Must use struct keyword since we do not reserve compiler space */
-	struct simple* example_struct = ( struct simple* )malloc( sizeof( struct simple ) );
+	simple* example_struct = ( simple* )malloc( sizeof( simple ) );
 
 	/* De-reference and assign data values */
 	example_struct->int_data = 1;
 	example_struct->double_data = -13.4;
 	
-	fprintf( stdout, "%d %lf\n", example_struct->int_data, example_struct->double_data );
+	fprintf( stdout, "%p %p\n", &example_struct, example_struct );
+	fprintf( stdout, "%d %p\n", example_struct->int_data, &(example_struct->int_data) );
+	fprintf( stdout, "%lf %p\n", example_struct->double_data, &(example_struct->double_data) );
 	
 	/* Free dynamically allocated memory */
 	free( example_struct );
 	
 	return 0;
 }
+
