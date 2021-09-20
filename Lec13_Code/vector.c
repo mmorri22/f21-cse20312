d7a0dc2c606a16fb79bbfda7751c8fd6f1d5d807
Matthew Morrison <mmorri22@nd.edu>
2021-09-19 21:59:58 -0400

    Lecture 13 Push

--- Lec13_Code/vector.c
+++ Lec13_Code/vector.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct vector{
-	
-	long unsigned int length;
-	long unsigned int capacity;
-	
-	int* the_array;
-	
-} vector;
+
 
 
 void constructor( vector* int_vector ){
 	
-	int_vector->length = 0;
-	int_vector->capacity = 0;
-	int_vector->the_array = NULL;
+	
+	
+	
 	
 }
 
 
 void destructor( vector* int_vector ){
 	
-	if( int_vector->the_array != NULL )
-		free( int_vector->the_array );
+	
+		
 	
 }
 
@@ -42,45 +35,46 @@ void print_array( vector* int_vector ){
 void insert( vector* int_vector, int insert_val ){
 	
 	// First, follow Dr. Morrison's Golden Rule of Pointers
-	if( int_vector->the_array == NULL ){
+	if(  ){
 		
 		// Set the array size to 1
-		int_vector->the_array = (int *)malloc( sizeof(int) );
+		
 		
 		// Set the capacity to 1
-		int_vector->capacity = 1;
+		
 		
 	}
 	
-	if( int_vector->length == int_vector->capacity ){
+	// If the length is equal to the capacity
+	if(  ){
 		
 		// Double the capacity
-		int_vector->capacity *= 2;
+		
 		
 		// Set a reference to the current array 
-		int* reference = int_vector->the_array;
+		
 		
 		// Allocate a new array to the array of the new capacity 
-		int_vector->the_array = (int *)malloc( sizeof(int) * int_vector->capacity );
 		
-		// Copy the old elements to the new elements 
-		long unsigned int iter;
-		for( iter = 0; iter < int_vector->length; ++iter ){
+		
+		// Copy the old elements to the new elements. Use the length
+		
+		
 			
-			int_vector->the_array[iter] = reference[iter];
 			
-		}
+			
+		
 		
 		// Free the old vector 
-		free( reference );
+		
 		
 	}
 	
 	// Set the initial element equal to insert_val
-	int_vector->the_array[ int_vector->length ] = insert_val;
+	
 	
 	// Increment the length
-	int_vector->length++;
+	
 	
 	return;
 	
@@ -90,10 +84,10 @@ void insert( vector* int_vector, int insert_val ){
 int main(){
 	
 	// malloc the struct  
-	vector* int_vector = (vector *)malloc( sizeof( vector ) );	
+	
 	
 	// Call the constructor 
-	constructor( int_vector );
+	
 	
 	// Keep looping 
 	char read_char = 'y';
@@ -104,10 +98,10 @@ int main(){
 		fprintf( stdout, "Enter an integer: ");
 		fscanf( stdin, "%d", &read_int );
 
-
-		insert( int_vector, read_int );
+		// Insert and print 
 		
-		print_array( int_vector );
+		
+		
 
 		// Flush the input buffer
 		getchar();
@@ -118,10 +112,10 @@ int main(){
 	}
 	
 	// Call the destructor 
-	destructor( int_vector );
+	
 	
 	// Free the vector 
-	free( int_vector );
+	
 	
 	return 0;
 	
