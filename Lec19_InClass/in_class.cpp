924be35f134faef9784c64c4b79633e5349e35c8
Matthew Morrison <mmorri22@nd.edu>
2021-10-04 08:46:44 -0400

    Lecture 19 Update

---
+++ Lec19_InClass/in_class.cpp
@@ -0,0 +1,15 @@
+/***********************************************
+ * File name: in_class.cpp
+ * 
+ * This file contains the main driver function 
+ * for the in-class coding opportunity
+ *********************************************/
+ 
+ 
+ int main( void ){
+	 
+	 
+	 
+	 
+	 return 0;
+ }
---
+++ Lec19_InClass/setup.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lec19_InClass/point.cpp
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lec19_InClass/point.h
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lec19_InClass/line.cpp
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lec19_InClass/line.h
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lec19_InClass/in_class.cpp
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lec19_InClass/Makefile
+rm setup.sh
