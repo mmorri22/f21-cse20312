c9351cb1b11dc7f31ca8a65d8529ee1c7d561cea
Matthew Morrison <mmorri22@nd.edu>
2021-10-04 08:39:35 -0400

    Lab 06 Final Push

---
+++ Lab06/Lab06_setup.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lab06/Stocks.cpp
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lab06/Stocks_1.cpp
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lab06/Makefile
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lab06/Product.cpp
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lab06/Product_Lab.cpp
+rm Lab06_setup.sh
