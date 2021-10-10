bbbe8caafc7ef5bffb261c9e518e1e20ee9f10d7
Matthew Morrison <mmorri22@nd.edu>
2021-10-10 13:54:54 -0400

    Lecture 22 In Class Push

---
+++ Lec22_InClass/Lec22_setup.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lec22_InClass/min_heap.h
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lec22_InClass/max_heap.h
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lec22_InClass/Makefile
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lec22_InClass/test_min_heap.cpp
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lec22_InClass/test_max_heap.cpp
+rm Lec22_InClass_setup.sh
---
+++ Lec22_InClass/min_heap.h
@@ -0,0 +1,11 @@
+#ifndef MIN_HEAP_H
+#define MIN_HEAP_H
+
+#include <vector>
+#include <iostream>
+
+#define VECTOR std::vector
+
+
+	
+#endif
