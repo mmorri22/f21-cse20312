d02c8a7e87e18b0b33c927423ff5ce1ebf30fb0b
Matthew Morrison <mmorri22@nd.edu>
2021-10-27 07:36:05 -0400

    Red Black Tree Push

---
+++ Lec26_Code/setup.sh
@@ -0,0 +1,38 @@
+mkdir src inc
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/Makefile
+make initialize
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/test.sh
+chmod a+rx test.sh
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/Prob1.cpp
+cp Prob1.cpp src/Prob1.cpp
+rm Prob1.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/Prob2.cpp
+cp Prob2.cpp src/Prob2.cpp
+rm Prob2.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/testRBTNode1.cpp
+cp testRBTNode1.cpp src/testRBTNode1.cpp
+rm testRBTNode1.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/testRBTNode2.cpp
+cp testRBTNode2.cpp src/testRBTNode2.cpp
+rm testRBTNode2.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/testRBTree1.cpp
+cp testRBTree1.cpp src/testRBTree1.cpp
+rm testRBTree1.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/testRBTree2.cpp
+cp testRBTree2.cpp src/testRBTree2.cpp
+rm testRBTree2.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/inc/RBTree1.h
+cp RBTree1.h inc/RBTree1.h
+rm RBTree1.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/inc/RBTree2.h
+cp RBTree2.h inc/RBTree2.h
+rm RBTree2.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/inc/RBTNode1.h
+cp RBTNode1.h inc/RBTNode1.h
+rm RBTNode1.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/inc/RBTNode2.h
+cp RBTNode2.h inc/RBTNode2.h
+rm RBTNode2.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/inc/RBTNode.h
+cp RBTNode.h inc/RBTNode.h
+rm RBTNode.h
---
+++ Lec26_Code/setup2.sh
@@ -0,0 +1,27 @@
+mkdir src inc
+rm Makefile
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/Makefile
+rm test.sh
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/test.sh
+chmod a+rx test.sh
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/Prob3.cpp
+cp Prob3.cpp src/Prob3.cpp
+rm Prob3.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/inc/RBTree.h
+cp RBTree.h inc/RBTree.h
+rm RBTree.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/RBTreeTest.cpp
+cp RBTreeTest.cpp src/RBTreeTest.cpp
+rm RBTreeTest.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/RBDelTest.cpp
+cp RBDelTest.cpp src/RBDelTest.cpp
+rm RBDelTest.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/treeTest.cpp
+cp treeTest.cpp src/treeTest.cpp
+rm treeTest.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/mapTest.cpp
+cp mapTest.cpp src/mapTest.cpp
+rm mapTest.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec22/src/multiSet.cpp
+cp multiSet.cpp src/multiSet.cpp
+rm multiSet.cpp
