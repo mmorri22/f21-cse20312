8cb80586002fc82241918ad11d084f439f1f2bbf
Matthew Morrison <mmorri22@nd.edu>
2021-11-02 09:30:22 -0400

    Lab 09 Update

---
+++ Lab09/Lab09_setup.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lab09/Lab09.h
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lab09/Lab09_sol.h
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lab09/Makefile
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20312/main/Lab09/InsertInt.cpp
+rm Lab09_setup.sh
